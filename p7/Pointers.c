9c8c40c8422d79dd731906653bc24b4940270d1c
vsapiens <erickfrank@icloud.com>
2020-02-21 11:43:09 -0600

    Finished until 7

---
+++ p7/Pointers.c
@@ -0,0 +1,17 @@
+//
+// Autor: Erick González
+// Matrícula: A01039859
+// Fecha: 18/02/2020
+//
+// Obtiene el valor y el tamaño de espacio que ocupan diferentes tipos de datos.
+
+int main(void)
+{
+    int *a = 4;
+    int *e = 3;
+    int *i = 1;
+    int *o = 0;
+    int *u = 2;
+
+    return 0;
+}
