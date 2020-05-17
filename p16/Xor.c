8e337181f170b196382d058926fd0a4729dbedc2
vsapiens <erickfrank@icloud.com>
2020-05-17 16:52:57 -0500

    Added p 16

--- p16/Xor.c
+++ p16/Xor.c
@@ -1,26 +1,34 @@
 //
 // Autor: Erick González
 // Matrícula: A01039859
-// Fecha: 27/03/2020
+// Fecha: 15/05/2020
 //
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
+    FILE *in, *out;
+
     int curr;
-    scanf("%d", &curr);
+    in = fopen("input.txt", "r");
+    out = fopen("output.txt", "w+");
+    fscanf(in, "%d", &curr);
     while (curr--)
     {
         int num, a, v;
         int s = 0;
 
-        scanf("%d", &num);
+        fscanf(in, "%d", &num);
         while (num--)
         {
-            scanf("%d %d", &a, &v);
+            fscanf(in, "%d %d", &a, &v);
             s += a - v;
         }
-        printf("%d\n", s);
+        fprintf(out, "%d\n", s);
     }
+    fclose(in);
+    fclose(out);
     return 0;
 }
